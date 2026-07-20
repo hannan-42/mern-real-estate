4a062f99fa379b0dc3354d3d8b25a0345c1fb860
Muhammad Hannan <hannannaeem24@gmail.com>
2026-07-21 01:01:40 +0500

    Connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('Database Connection Error:', err));
 
 const app = express()
 
